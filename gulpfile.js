6a8b4aee3c555b77b480c03950d6f227fd8c1a15
palerdot <palerdot@gmail.com>
2016-07-14 22:00:59 +0530

    [minor] gh-page auto build log tweak

--- gulpfile.js
+++ gulpfile.js
@@ -698,7 +698,7 @@ gulp.task( "clean-gh-pages", function (cb) {
 
 gulp.task( "deploy-gh-pages", shell.task([
     "tar -xvzf web.tar.gz --strip-components=1 && rm web.tar.gz",
-    "echo [auto-web-build] `date +%Y-%m-%d\ %H:%M:%S` > build_log",
+    "echo [auto-web-build] `date +%Y-%m-%d\ %H:%M:%S` >> build_log",
     'git add --all && git commit -m "[auto-web-build] `date +%Y-%m-%d\ %H:%M:%S`"'
 ]));
 
