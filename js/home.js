ad5dd284881493badf7a06fda253fbd8f8d17929
palerdot <palerdot@gmail.com>
2016-09-16 14:31:50 +0530

    [new home]

--- js/home.js
+++ js/home.js
@@ -20,4 +20,22 @@ $(document).ready( function () {
 
     } );
 
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+
 } );
